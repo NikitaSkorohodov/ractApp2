d928f8b93dc00079de0a21ed5ce05e46cbbd8f59
NikitaSkorohodov <113201752+NikitaSkorohodov@users.noreply.github.com>
2023-10-04 14:36:57 +0300

    Add files via upload

---
+++ React_2_IceAndFire/src/components/app/app.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import { Col, Row, Container } from 'reactstrap';
+import Header from '../header';
+import RandomChar from '../randomChar';
+import ErrorMessage from '../errorMessage';
+import { CharacterPage, BooksPage, HousesPage, BooksItem } from '../pages';
+import gotService from '../../services/gotService';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './app.css';
+
+export default class App extends Component {
+    gotService = new gotService();
+
+    state = {
+        showRandomChar: true,
+        error: false,
+        selectedHouse: 20
+    };
+
+    componentDidCatch(error, info) {
+        console.log('error', error, info);
+        this.setState({
+            error: true
+        });
+    }
+
+    toggleRandomChar = () => {
+        this.setState((state) => {
+            return {
+                showRandomChar: !state.showRandomChar
+            };
+        });
+    };
+
+    render() {
+        const char = this.state.showRandomChar ? <RandomChar /> : null;
+        if (this.state.error) {
+            return <ErrorMessage />;
+        }
+
+        return (
+            <Router>
+                <div className="app">
+                    <Container>
+                        <Header />
+                    </Container>
+                    <Container>
+                        <Row>
+                            <Col lg={{ size: 5, offset: 0 }}>
+                                {char}
+                                <button
+                                    className="toggle-btn"
+                                    onClick={this.toggleRandomChar}>Toggle random character</button>
+                            </Col>
+                        </Row>
+                        <Route path='/' exact component={() => <h1>Welcome</h1>} />
+                        <Route path='/characters' component={CharacterPage} />
+                        <Route path='/books' exact component={BooksPage} />
+                        <Route path='/books/:id' component={({ match }) => {
+                            const { id } = match.params;
+                            return <BooksItem bookId={id} />;
+                        }} />
+                        <Route path='/houses' component={HousesPage} />
+                    </Container>
+                </div>
+            </Router>
+        );
+    }
+}
